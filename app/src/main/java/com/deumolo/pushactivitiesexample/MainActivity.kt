ef1d0d3694a26b413c81276c4f1ab274cb357dfc
Daniel <deumolo@gmail.com>
2023-08-05 10:21:30 -0600

    Added button behavior to main activity

--- app/src/main/java/com/deumolo/pushactivitiesexample/MainActivity.kt
+++ app/src/main/java/com/deumolo/pushactivitiesexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.deumolo.pushactivitiesexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var pushBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pushBtn = findViewById(R.id.pushBtn)
+
+        pushBtn.setOnClickListener {
+            println("Btn pressed")
+        }
     }
 }
